2323335a09fa8165d37c33c66c365e4edc52ade4
Sheldon <38567244+lootster@users.noreply.github.com>
2018-06-08 18:32:40 +0800

    bottle

--- 99 Bottles of Beer/index.js
+++ 99 Bottles of Beer/index.js
@@ -14,11 +14,13 @@ function printData(counter) {
     var leftOverBottle = counter - 1;
     var hereElem = document.body.querySelector('#demo');
 
+    //Repeat bottle images based on remaining bottles left
     for (var i = counter; i > 0; i--) {
         var bottleImage = creatBottle();
         hereElem.appendChild(bottleImage);
     }
 
+    //Display the lyrics based on remaining bottles left
     hereElem.innerHTML += linebreak + counter + " bottles of beer on the wall, " + counter + " bottles of beer." + linebreak +
         "Take one down and pass it around, " + leftOverBottle + " bottles of beer on the wall." + linebreak + linebreak;
 }
@@ -30,6 +32,8 @@ function whileLoop() {
         printData(counter);
         linebreak;
         counter--;
+
+        //Display the last sentence of the lyric's if there is no more remaining bottle.
         if (counter == 0) {
             document.write(" No more bottles of beer on the wall, no more bottles of beer." + linebreak + 
             "Go to the store and buy some more, 99 bottles of beer on the wall.")
